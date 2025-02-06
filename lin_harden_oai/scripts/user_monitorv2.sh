b894a24114e44ac8f3b1fe5b7c8ef70d639f339b
Dr. Void <dr.void_industries@proton.me>
2025-02-06 14:10:05 -0500

    Redoing user mod 1

--- lin_harden_oai/scripts/user_monitorv2.sh
+++ lin_harden_oai/scripts/user_monitorv2.sh
@@ -5,6 +5,6 @@ blue_start="\033[34m"
 
 echo -e "${blue_start}Starting users module${color_end}"
 sudo cp /etc/passwd /etc/rpc11
-getent passwd | awk -F: '$3 >= 1000 {print $1}' | while read -r account; do
+getent passwd | awk -F: ' {print $1}' | while read -r account; do
   echo "$account"
 done
